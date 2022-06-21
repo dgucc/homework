fb5f94ac8cfb3b70e556e9bf67dd185e0a3ecd3c
David Gucciardi <GucciardiD@ccrek.be>
2022-06-21 16:36:12 +0200

    init

---
+++ homework/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebPub")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rekenhof - Cour des comptes")]
+[assembly: AssemblyProduct("WebPub")]
+[assembly: AssemblyCopyright("Copyright © Rekenhof - Cour des comptes 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dcfceda9-3036-479e-85e4-9c0cbfc52037")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ homework/resources/sql/DDL_webpub.sql
@@ -0,0 +1,19 @@
+﻿-- SQLCMD Mode
+:setvar DatabaseName "WebPub"
+
+USE master
+GO
+
+IF EXISTS (SELECT [name] FROM [master].[sys].[databases] WHERE [name] = N'$(DatabaseName)')
+    DROP DATABASE $(DatabaseName);
+GO
+
+CREATE DATABASE [WebPub];
+GO
+
+USE $(DatabaseName);
+GO
+
+CREATE SCHEMA [Thot] AUTHORIZATION [dbo];
+GO
+
