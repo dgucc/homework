053933f3a1f9b44d7f4a99dc28fca1e57dbc410a
dgucc <djvu.guchz@gmail.com>
2024-11-25 12:26:30 +0100

    Sysout declaration xml after persisting to get generated Id's

--- hj3_sandbox/src/main/java/minfin/App.java
+++ hj3_sandbox/src/main/java/minfin/App.java
@@ -2,9 +2,13 @@ package minfin;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import javax.xml.namespace.QName;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -12,6 +16,7 @@ import jakarta.persistence.Persistence;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBElement;
 import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 
 import be.fgov.minfin.beps13.notification.v1.ObjectFactory;
@@ -19,8 +24,8 @@ import be.fgov.minfin.beps13.notification.v1.Declaration275CBCNOTType;
 
 public class App {
    private static JAXBContext jaxbContext;
-	private static ObjectFactory objectFactory;
-	private static EntityManagerFactory entityManagerFactory;
+   private static ObjectFactory objectFactory;
+   private static EntityManagerFactory entityManagerFactory;
 
    public static void main(String[] args) {
       System.out.println("*** Started main ***");
@@ -31,8 +36,9 @@ public class App {
 
          /* sample unmarshalling */
          final Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-         // final Object object = unmarshaller.unmarshal(new File("src/sample/samples/BEPS13_Notification_sample_v1.5.xml"));
-         final Object object = unmarshaller.unmarshal(new File("src/test/samples/sample.xml"));
+         // final Object object = unmarshaller.unmarshal(new
+         // File("src/sample/samples/BEPS13_Notification_sample_v1.5.xml"));
+         final Object object = unmarshaller.unmarshal(new File("src/test/samples/BEPS13_Notification_test_v1.5.xml"));
          final Declaration275CBCNOTType sample = ((JAXBElement<Declaration275CBCNOTType>) object).getValue();
 
          /* sample persisting after unmarshalling */
@@ -51,7 +57,8 @@ public class App {
             }
          }
 
-         entityManagerFactory = Persistence.createEntityManagerFactory("be.fgov.minfin.beps13.notification.v1:oecd.ties.isocbctypes.v1", persistenceProperties);
+         entityManagerFactory = Persistence.createEntityManagerFactory(
+               "be.fgov.minfin.beps13.notification.v1:oecd.ties.isocbctypes.v1", persistenceProperties);
 
          final EntityManager entityManager = entityManagerFactory.createEntityManager();
          entityManager.getTransaction().begin();
@@ -62,11 +69,25 @@ public class App {
          entityManager.persist(sample);
          entityManager.getTransaction().commit();
 
+         /* marshalling declaration275CBCNOT temp/export/<bce>_<declarationId>.xml */
+         jaxbContext = JAXBContext.newInstance(Declaration275CBCNOTType.class);
+         final Marshaller marshaller = jaxbContext.createMarshaller();
+         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+         marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+
+         marshaller.marshal(
+               new JAXBElement<Declaration275CBCNOTType>(
+                     new QName("be.fgov.minfin.beps13.entity.notification.v1_51", "Declaration275CBCNOT"),
+                     Declaration275CBCNOTType.class,
+                     sample),
+               // new FileOutputStream(new File("target/" + String.format("%010d",
+               // sample.getDeclarer().getCompanyNumber()) + "_" + String.format("%010d",
+               // sample.getHjid()) + "_notif.xml")));
+               System.out);
+
          entityManager.close();
 
-         System.out.println("CompanyNumber : " + sample.getDeclarer().getCompanyNumber());
-         System.out.println("CountryCodes :" + sample.getDeclarer().getCompanyName());
-         System.out.println("sample.getId() : " + sample.getHjid());
+         entityManager.close();
 
       } catch (JAXBException e) {
          System.out.println(e.getMessage());
@@ -74,5 +95,4 @@ public class App {
       System.out.println("*** Terminated ***");
    }
 
-   
 }
